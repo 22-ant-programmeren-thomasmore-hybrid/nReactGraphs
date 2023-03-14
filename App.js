7d89a42505194a7ffe7719fbf1b8ef23204bdecb
hbkYuna <strawhatnxahx@gmail.com>
2023-03-15 00:53:34 +0100

    Slider 1,2 en 3 (inverted slider) met aparte kleuren

--- App.js
+++ App.js
@@ -4,17 +4,39 @@ import Slider from '@react-native-community/slider';
 
 const App = () => {
     return (
-        <View>
-            <Text>Slider 1</Text>
+        <View style={styles.container}>
+            <Text style={styles.sliderText}>Slider 1</Text>
             <Slider
+                style={styles.slider1}
                 minimumValue={0}
                 maximumValue={1}
-                minimumTrackTintColor="#000000"
+                minimumTrackTintColor="#FFFFFF"
                 maximumTrackTintColor="#000000"
+            />
+            <Text style={styles.sliderText}>Slider 2</Text>
+            <Slider
+                style={styles.slider2}
+                minimumValue={0}
+                maximumValue={10}
+                minimumTrackTintColor="#00FF00"
+                maximumTrackTintColor="#000000"
+            />
+            <Text style={styles.sliderText}>Slider 3</Text>
+            <Slider
+                style={styles.slider3}
+                minimumValue={-50}
+                maximumValue={50}
+                minimumTrackTintColor="#0000FF"
+                maximumTrackTintColor="#000000"
+                inverted={true}
             />
         </View>
     );
 };
 
+const styles = StyleSheet.create({
+
+
+});
 
 export default App;
